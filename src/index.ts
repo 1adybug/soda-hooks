8eac9629615945e9199574135845f14c77830df5
陆子虚 <11374106+zixulu@user.noreply.gitee.com>
2024-02-29 16:08:01 +0800

    添加 useAsync

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import { Dispatch, SetStateAction, useMemo, useState } from "react"
+import type { DependencyList, Dispatch, SetStateAction } from "react"
+import { useEffect, useMemo, useState } from "react"
 
 export interface Storage {
     getItem(key: string): string | null
@@ -10,14 +11,14 @@ export interface StorageStateHookStringConfig {
     key: string
     storage: Storage
     parser?: (value: string | null) => string | null
-    serializer?: (value: string | null) => string
+    serializer?: (value: string | null) => string | null
 }
 
 export interface StorageStateHookDataConfig<Data> {
     key: string
     storage: Storage
     parser: (value: string | null) => Data
-    serializer: (value: Data) => string
+    serializer: (value: Data) => string | null
 }
 
 export function useStorageState(config: StorageStateHookStringConfig): [string | null, Dispatch<SetStateAction<string | null>>]
@@ -42,16 +43,50 @@ export function useStorageState<Data>(config: StorageStateHookStringConfig | Sto
     return [state, setState]
 }
 
-export function useSessionStorageState(key: string): [string | null, Dispatch<SetStateAction<string | null>>]
-export function useSessionStorageState<Data>(config: Omit<StorageStateHookDataConfig<Data>, "storage">): [Data, Dispatch<SetStateAction<Data>>]
-export function useSessionStorageState<Data>(keyOrConfig: string | Omit<StorageStateHookDataConfig<Data>, "storage">) {
-    const config = typeof keyOrConfig === "string" ? { key: keyOrConfig, storage: sessionStorage } : { ...keyOrConfig, storage: sessionStorage }
-    return useStorageState(config as any)
+export function useLocalStorageState(key: string): [string | null, Dispatch<SetStateAction<string | null>>]
+export function useLocalStorageState(key: string, config: Omit<StorageStateHookStringConfig, "storage" | "key">): [string | null, Dispatch<SetStateAction<string | null>>]
+export function useLocalStorageState<Data>(key: string, config: Omit<StorageStateHookDataConfig<Data>, "storage" | "key">): [Data, Dispatch<SetStateAction<Data>>]
+export function useLocalStorageState<Data>(key: string, config?: Omit<StorageStateHookStringConfig | StorageStateHookDataConfig<Data>, "storage" | "key">) {
+    const c = { key, storage: localStorage, ...config }
+    return useStorageState(c as any)
 }
 
-export function useLocalStorageState(key: string): [string | null, Dispatch<SetStateAction<string | null>>]
-export function useLocalStorageState<Data>(config: Omit<StorageStateHookDataConfig<Data>, "storage">): [Data, Dispatch<SetStateAction<Data>>]
-export function useLocalStorageState<Data>(keyOrConfig: string | Omit<StorageStateHookDataConfig<Data>, "storage">) {
-    const config = typeof keyOrConfig === "string" ? { key: keyOrConfig, storage: localStorage } : { ...keyOrConfig, storage: localStorage }
-    return useStorageState(config as any)
+export function useSessionStorageState(key: string): [string | null, Dispatch<SetStateAction<string | null>>]
+export function useSessionStorageState(key: string, config: Omit<StorageStateHookStringConfig, "storage" | "key">): [string | null, Dispatch<SetStateAction<string | null>>]
+export function useSessionStorageState<Data>(key: string, config: Omit<StorageStateHookDataConfig<Data>, "storage" | "key">): [Data, Dispatch<SetStateAction<Data>>]
+export function useSessionStorageState<Data>(key: string, config?: Omit<StorageStateHookStringConfig | StorageStateHookDataConfig<Data>, "storage" | "key">) {
+    const c = { key, storage: sessionStorage, ...config }
+    return useStorageState(c as any)
+}
+
+function isAsyncGenerator(value: AsyncGenerator<void, void, void> | Promise<void>): value is AsyncGenerator<void, void, void> {
+    return typeof (value as AsyncGenerator<void, void, void>)[Symbol.asyncIterator] === "function"
+}
+
+export function useAsync(effect: () => AsyncGenerator<void, void, void> | Promise<void>, deps?: DependencyList): void
+export function useAsync(effect: () => AsyncGenerator<void, void, void> | Promise<void>, callback: () => void, deps?: DependencyList): void
+export function useAsync(effect: () => AsyncGenerator<void, void, void> | Promise<void>, callbackOrDeps?: (() => void) | DependencyList, deps?: DependencyList) {
+    const dependencyList = typeof callbackOrDeps === "function" ? deps : callbackOrDeps
+    useEffect(() => {
+        const generator = effect()
+        let stop = false
+        async function run() {
+            if (isAsyncGenerator(generator)) {
+                while (true) {
+                    const result = await generator.next()
+                    if (result.done || stop) {
+                        generator.return()
+                        break
+                    }
+                }
+            }
+        }
+        run()
+        return () => {
+            stop = true
+            if (typeof callbackOrDeps === "function") {
+                callbackOrDeps()
+            }
+        }
+    }, dependencyList)
 }
